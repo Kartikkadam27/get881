566243b28e7697eed58fd2c60d14e60defea2a23
Kartikkadam27 <37414955+Kartikkadam27@users.noreply.github.com>
2020-12-17 13:46:12 +0530

    latest commit
    Added diff, multiply method

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,15 @@ class Calculator{
   public static int add(int a, int b){
       return a+b;
     }
+   public static int diff(int a, int b){
+      return a-b;
+    }
+   public static int mul(int a, int b){
+      return a*b;
+    }
+   public static int div(int a, int b){
+      return a/b;
+    }
   public static void main(String args[])
     {
       System.out.println(add(10,20));
